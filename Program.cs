183992ef5cebb495ebe9ef9305c332c8f4713004
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-18 21:30:51 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_for03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//右側對齊星形三角形
+			//     *
+			//    **
+			//   ***
+			//  ****
+			// *****
+
+			for (int i = 1; i <=5 ; i++)
+			{
+				string countOfStar = new string('*', i);
+				string countOfEmpty = new string(' ', 6-i);
+				Console.WriteLine(countOfEmpty+countOfStar);
+			}
+			Console.ReadKey();
+		}
+	}
+}
